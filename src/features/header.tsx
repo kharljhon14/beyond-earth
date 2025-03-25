193204905b167d8b726f73a38b3dd43773f0725d
karl <enriquezkharl14@gmail.com>
2025-03-25 16:04:45 +0800

    [Style] Update header

--- src/features/header.tsx
+++ src/features/header.tsx
@@ -6,7 +6,7 @@ export default function Header() {
 
   return (
     <header className="bg-white shadow-md p-6 lg:py-2 lg:px-8 md:rounded-2xl md:my-6 lg:flex justify-between lg:mb-8">
-      <div className="flex justify-between items-center w-full">
+      <div className="flex justify-between items-center">
         <div>
           <a href="">BeyondEarth</a>
         </div>
@@ -24,7 +24,7 @@ export default function Header() {
           open ? ' h-[16rem]' : 'h-0'
         } overflow-hidden transition-all duration-300 lg:h-auto lg:overflow-auto lg:flex justify-between w-full`}
       >
-        <nav className="flex flex-col lg:flex-row gap-x-8 gap-y-4 items-center my-6">
+        <nav className="flex flex-col lg:flex-row gap-x-8 gap-y-4 items-center my-6 mx-auto">
           <a href="">Home</a>
           <a href="">Projects</a>
           <a href="">News</a>
